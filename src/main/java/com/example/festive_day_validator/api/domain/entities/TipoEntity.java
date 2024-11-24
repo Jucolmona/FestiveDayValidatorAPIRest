20648b5a662adb7b6056ca17126a7275bf508ecf
JUAN CAMILO MONA LUJAN <jucmonalu@unal.edu.co>
2024-11-23 20:50:21 -0500

    Create the Type Entity in the domain layer

--- src/main/java/com/example/festive_day_validator/api/domain/entities/TipoEntity.java
+++ src/main/java/com/example/festive_day_validator/api/domain/entities/TipoEntity.java
@@ -1,3 +1,26 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "tipo")
 public class TipoEntity {
-    
+
+    @Id
+    @Column(name="id")
+    private int id;
+
+    @column(name="tipo", length=100)
+    private String type;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }

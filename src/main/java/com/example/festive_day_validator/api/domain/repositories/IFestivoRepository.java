9d1555061c3e617e639773d293f435f12949c3ce
JUAN CAMILO MONA LUJAN <jucmonalu@unal.edu.co>
2024-11-25 20:46:30 -0500

    Update Interface FestivoRepository

--- src/main/java/com/example/festive_day_validator/api/domain/repositories/IFestivoRepository.java
+++ src/main/java/com/example/festive_day_validator/api/domain/repositories/IFestivoRepository.java
@@ -1,9 +1,14 @@
 package com.example.festive_day_validator.api.domain.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IFestivoRepository extends JpaRepository<FestivoEntity, Integer> {
+    List<FestivoEntity> findAll();
+    Optional<FestivoEntity> finfindBy(Integer id);
+    List<FestivoEntity> findByTypeFestive(Integer type);
 }
 
